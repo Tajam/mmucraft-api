e0e050df0c18d282bf7e4e34ea9d03a00915fbf9
TajamHeng <oojinheng@gmail.com>
2020-06-27 02:58:45 +0800

    added uuid api

---
+++ routes/uuid.read.route.js
@@ -0,0 +1,23 @@
+/**
+ * get uuid from supplied player name
+ * request:
+ *   - name: player name
+ * response:
+ *   - uuid: the generated uuid
+ */
+
+import { uuidConstruct } from '../utils/hash/uuid.js'
+
+export const controller = (req, res) => {
+  const { name } = req.body
+  if (!name || name === '') {
+    res.status(400).send('must supply a non-empty string')
+    return
+  }
+  try {
+    const uuid = uuidConstruct(name)
+    res.status(200).json({ uuid: uuid })
+  } catch (e) {
+    res.status(500).send('error')
+  }
+}
