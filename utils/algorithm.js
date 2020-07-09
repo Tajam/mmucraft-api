b20d504aa9bfdeb35aada9c31601e29ae7f48205
TajamHeng <oojinheng@gmail.com>
2020-07-09 23:31:26 +0800

    fix scoping error

--- utils/algorithm.js
+++ utils/algorithm.js
@@ -3,8 +3,10 @@ import md5 from 'md5'
 
 export const shuffle = (list) => {
   for (let i = list.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    [list[i], list[j]] = [list[j], list[i]]
+    let j = Math.floor(Math.random() * (i + 1))
+    let temp = list[j]
+    list[j] = list[i]
+    list[i] = temp
   }
 }
 
